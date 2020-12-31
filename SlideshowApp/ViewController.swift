13cd2fc8b24cecddba6a24c2eb11318d768ed421
tomoya.seki <te4090st@gmail.com>
2020-12-31 10:15:20 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by 関智矢 on 2020/12/31.
+//  Copyright © 2020 tomoya.seki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
